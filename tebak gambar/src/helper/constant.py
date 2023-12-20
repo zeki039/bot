6571ed625afa3798706010b3617fd6f71b6669a6
zeki039 <81940045+zeki039@users.noreply.github.com>
2023-12-20 12:58:50 +0700

    Add files via upload

---
+++ tebak gambar/src/helper/constant.py
@@ -0,0 +1,22 @@
+class Constant(object):
+    GROUP_ALERT = """
+<i>Aku hanya dapat dimainkan pada group dengan tipe <b>supergroup.</b>
+Tekan tombol <b>Tutorial</b> dibawah ini untuk melihat bagaimana cara mengubah <b>group</b> menjadi <b>supergroup</b></i>
+"""
+    NOT_ADMIN_ALERT = """
+⚠ <b>Alert</b> ⚠
+<i>Jadikan aku sebagai <b>admin</b> dan beri aku hak untuk <b>menghapus pesan.</b></i>
+"""
+    ABOUT = """
+🤖 <b>Nama :</b> <a href=\"t.me/tebakgambarid_bot\">Tebak Gambar</a>
+
+🧑‍💻 <b>Developer :</b> <a href=\"t.me/piscesxboy\">zeki</a>
+
+"""
+    HELP = """
+Ganti sendiri textnya!
+"""
+    START = """
+Langsung saja yuk, kita bermain... kamu dapat memulainya dengan
+menekan tombol Petunjuk dibawah untuk membaca tentang cara menggunakanku. Enjoy!!!
+"""
